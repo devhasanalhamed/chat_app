5a51876ac01dabe2ea63e6954b07f2da6b4f5490
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-01-28 16:38:03 +0300

    Update chat_screen.dart

--- lib/features/chat/ui/screen/chat_screen.dart
+++ lib/features/chat/ui/screen/chat_screen.dart
@@ -1,3 +1,6 @@
+import 'package:chat_app/features/chat/ui/widget/chat_messages.dart';
+import 'package:chat_app/features/chat/ui/widget/new_message.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -6,8 +9,23 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('FlutterChat')),
-      body: const Center(child: Text('Loggid in!')),
+      appBar: AppBar(
+        title: const Text('FlutterChat'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: const Icon(Icons.exit_to_app),
+          ),
+        ],
+      ),
+      body: const Column(
+        children: [
+          Expanded(child: ChatMessages()),
+          NewMessage(),
+        ],
+      ),
     );
   }
 }
