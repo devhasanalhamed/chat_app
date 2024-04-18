1a80157d3e1fc9f7d0a0a375d09aa5a96b3c3211
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-04-18 12:35:41 +0300

    Create navigation_service.dart

---
+++ lib/core/services/navigation_service.dart
@@ -0,0 +1,26 @@
+import 'package:chat_app/view/screen/login_screen.dart';
+import 'package:flutter/material.dart';
+
+class NavigationService {
+  late GlobalKey<NavigatorState> _navigatorKey;
+
+  GlobalKey<NavigatorState> get navigatorKey => _navigatorKey;
+
+  final Map<String, Widget Function(BuildContext)> _routes = {
+    "/login": (context) => const LoginScreen(),
+  };
+
+  Map<String, Widget Function(BuildContext)> get routes => _routes;
+
+  NavigationService() {
+    _navigatorKey = GlobalKey<NavigatorState>();
+  }
+
+  void pushNamed(String routeName) =>
+      _navigatorKey.currentState?.pushNamed(routeName);
+
+  void pushReplacementNamed(String routeName) =>
+      _navigatorKey.currentState?.pushReplacementNamed(routeName);
+
+  void goBack() => _navigatorKey.currentState?.pop();
+}
