6f20a3a86426e7893e8001daba3f3a9ffd2dcf94
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-04-20 09:42:30 +0300

    Update main.dart
    implement the new navigation service and declare routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
+import 'package:chat_app/core/services/navigation_service.dart';
 import 'package:chat_app/core/utils/firebase_utils.dart';
 import 'package:chat_app/core/utils/service_locator_utils.dart';
 import 'package:chat_app/view/screen/login_screen.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   await setup();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 Future<void> setup() async {
@@ -17,13 +19,20 @@ Future<void> setup() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final GetIt _getIt = GetIt.instance;
+
+  late NavigationService _navigationService;
+
+  MyApp({super.key}) {
+    _navigationService = _getIt<NavigationService>();
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      navigatorKey: _navigationService.navigatorKey,
       title: 'Chat-app',
       theme: ThemeData(
         textTheme: GoogleFonts.montserratTextTheme(),
@@ -33,6 +42,8 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const LoginScreen(),
+      initialRoute: '/login',
+      routes: _navigationService.routes,
     );
   }
 }
