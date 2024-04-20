2e313aab6fcecc6f2a829934b41324fde008a685
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-04-20 10:40:13 +0300

    Update homepage.dart

--- lib/view/screen/homepage.dart
+++ lib/view/screen/homepage.dart
@@ -1,13 +1,49 @@
+import 'package:chat_app/core/services/auth_service.dart';
+import 'package:chat_app/core/services/navigation_service.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  final GetIt _getIt = GetIt.instance;
+
+  late AuthService _authService;
+  late NavigationService _navigationService;
+  @override
+  void initState() {
+    _authService = _getIt.get<AuthService>();
+    _navigationService = _getIt.get<NavigationService>();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Messages'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              final bool result = await _authService.logOut();
+              if (result) {
+                _navigationService.pushReplacementNamed('/login');
+                debugPrint('user has been signed out');
+              } else {
+                debugPrint('logout result is false');
+              }
+            },
+            icon: const Icon(
+              Icons.logout,
+              color: Colors.red,
+            ),
+          ),
+        ],
       ),
     );
   }
