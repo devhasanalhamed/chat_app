40e3efda8a3c281437f783b7e33cee36674924a9
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-04-21 13:35:38 +0300

    Update firebase_utils.dart
    implement firebase options

--- lib/core/utils/firebase_utils.dart
+++ lib/core/utils/firebase_utils.dart
@@ -1,15 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart' show Firebase;
+import 'package:chat_app/firebase_options.dart';
 
 Future<void> setUpFirebase() async {
   await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey:
-          "AIzaSyBsofrrkxylGOmoglvyEWPp2gjuBDIq-TA", // paste your api key here
-      appId:
-          "1:567490514774:android:b07f4df8b44d68597410f7", //paste your app id here
-      messagingSenderId: "567490514774", //paste your messagingSenderId here
-      projectId: "chat-app-fced7", //paste your project id here
-      storageBucket: 'com.chat-app.dev',
-    ),
+    options: DefaultFirebaseOptions.currentPlatform,
   );
 }
