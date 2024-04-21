76db33534efdaf54b24acb4f7f86e41fc98e3c25
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-04-21 17:10:56 +0300

    Update database_service.dart

--- lib/core/services/database_service.dart
+++ lib/core/services/database_service.dart
@@ -1,13 +1,18 @@
 import 'package:chat_app/app/data/model/user_profile.dart';
+import 'package:chat_app/core/services/auth_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get_it/get_it.dart';
 
 class DatabaseService {
+  final GetIt _getIt = GetIt.instance;
   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
 
   CollectionReference? _usersCollection;
+  late AuthService _authService;
 
   DatabaseService() {
     _setupCollectionReferences();
+    _authService = _getIt.get<AuthService>();
   }
 
   void _setupCollectionReferences() {
@@ -24,5 +29,14 @@ class DatabaseService {
     required UserProfile userProfile,
   }) async {
     _usersCollection?.doc(userProfile.uid).set(userProfile);
+  }
+
+  Stream<QuerySnapshot<UserProfile>> getUserProfile() {
+    return _usersCollection
+        ?.where(
+          'uid',
+          isNotEqualTo: _authService.user!.uid,
+        )
+        .snapshots() as Stream<QuerySnapshot<UserProfile>>;
   }
 }
