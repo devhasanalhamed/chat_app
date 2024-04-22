c6a8832702adb5c70ae67d1b975d8b636fed3bf1
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-04-22 16:08:48 +0300

    Update storage_service.dart

--- lib/core/services/storage_service.dart
+++ lib/core/services/storage_service.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:path/path.dart' as path;
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:path/path.dart' as path;
 
 class StorageService {
   final FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
@@ -28,5 +28,22 @@ class StorageService {
       debugPrint('upload file error: $e');
     }
     return null;
+  }
+
+  Future<String?> uploadImageToChat({
+    required File file,
+    required String chatId,
+  }) async {
+    Reference fileRef = _firebaseStorage.ref('chats/$chatId').child(
+        '${DateTime.now().toIso8601String()}${path.extension(file.path)}');
+    UploadTask task = fileRef.putFile(file);
+    return task.then(
+      (i) {
+        if (i.state == TaskState.success) {
+          return fileRef.getDownloadURL();
+        }
+        return null;
+      },
+    );
   }
 }
