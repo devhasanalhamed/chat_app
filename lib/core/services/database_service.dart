f335fd54535b0a92739bd03b1d7c2b64183035ab
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-04-22 16:08:41 +0300

    Update database_service.dart

--- lib/core/services/database_service.dart
+++ lib/core/services/database_service.dart
@@ -1,5 +1,8 @@
+import 'package:chat_app/app/data/model/chat.dart';
+import 'package:chat_app/app/data/model/message.dart';
 import 'package:chat_app/app/data/model/user_profile.dart';
 import 'package:chat_app/core/services/auth_service.dart';
+import 'package:chat_app/core/utils/generate_chat_id.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get_it/get_it.dart';
 
@@ -8,6 +11,7 @@ class DatabaseService {
   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
 
   CollectionReference? _usersCollection;
+  CollectionReference? _chatCollection;
   late AuthService _authService;
 
   DatabaseService() {
@@ -22,6 +26,14 @@ class DatabaseService {
                 snapshot.data()!,
               ),
               toFirestore: (userProfile, options) => userProfile.toJson(),
+            );
+
+    _chatCollection =
+        _firebaseFirestore.collection('chats').withConverter<Chat>(
+              fromFirestore: (snapshot, options) => Chat.fromJson(
+                snapshot.data()!,
+              ),
+              toFirestore: (chat, options) => chat.toJson(),
             );
   }
 
@@ -38,5 +50,49 @@ class DatabaseService {
           isNotEqualTo: _authService.user!.uid,
         )
         .snapshots() as Stream<QuerySnapshot<UserProfile>>;
+  }
+
+  Future<bool> checkChatExists(String uid1, String uid2) async {
+    String chatId = generateChatId(uid1: uid1, uid2: uid2);
+    final result = await _chatCollection?.doc(chatId).get();
+    if (result != null) {
+      return result.exists;
+    } else {
+      return false;
+    }
+  }
+
+  Future<void> createNewChat(String uid1, String uid2) async {
+    String chatId = generateChatId(uid1: uid1, uid2: uid2);
+    final docRef = _chatCollection!.doc(chatId);
+    final chat = Chat(
+      id: chatId,
+      participants: [uid1, uid2],
+      messages: [],
+    );
+
+    await docRef.set(chat);
+  }
+
+  Future<void> sendChatMessage(
+      String uid1, String uid2, Message message) async {
+    final String chatId = generateChatId(uid1: uid1, uid2: uid2);
+    final docRef = _chatCollection!.doc(chatId);
+
+    docRef.update(
+      {
+        "messages": FieldValue.arrayUnion(
+          [
+            message.toJson(),
+          ],
+        ),
+      },
+    );
+  }
+
+  Stream<DocumentSnapshot<Chat>> getChatData(String uid1, String uid2) {
+    final String chatId = generateChatId(uid1: uid1, uid2: uid2);
+    return _chatCollection?.doc(chatId).snapshots()
+        as Stream<DocumentSnapshot<Chat>>;
   }
 }
